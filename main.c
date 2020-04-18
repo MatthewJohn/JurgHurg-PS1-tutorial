89c0d57591638d7e288630a5f512b5348d9b3c4e
Matthew John <matthew@dockstudios.co.uk>
2020-04-18 15:53:09 +0100

    Update main.c for second video

--- main.c
+++ main.c
@@ -8,11 +8,19 @@
 #include "constants.h"
 #include "controller.h"
 
+const int START_X = 160, START_Y = 128;
 
-
-float angle;
+float angle, c_ang, s_ang;
 float px, py;
-Line player, pov;
+float lx1, ly1, lx2, ly2;
+float tx1, ty1, tx2, ty2, tz1, tz2;
+Line player;
+Line line;
+
+void initialize();
+void update();
+void display();
+void moveSurroundings();
 
 int main() {
 	initialize();
@@ -20,6 +28,7 @@ int main() {
 	while(1) {
 		draw();
 		update();
+		moveSurroundings();
 		display();
 	}
 	
@@ -33,8 +42,17 @@ void initialize() {
 	setBackgroundColor(createColor(0,0,0));
 	
 	// Set intial coordinates of the player
-	px = 155; py = 155;
-	player = createLine(createColor(255,255,255), px, py, px, py);
+	angle = 0;
+	px = START_X;
+	py = START_Y;
+	
+	lx1 = START_X + 20;
+	ly1 = START_Y - 30;
+	lx2 = START_X + 20;
+	ly2 = START_Y + 30;
+	
+	player = createLine(createColor(255,255,255), px, py, px, py-5);
+	line = createLine(createColor(255,255,255), lx1, ly1, lx2, ly2);
 }
 
 void update() {
@@ -42,12 +60,12 @@ void update() {
 
     // Direction buttons
     if (padCheck(Pad1Up)) {
-		px = px + (cos(angle)*5);
-		py = py + (sin(angle)*5);
+		px = px + (c_ang*5);
+		py = py + (s_ang*5);
 	}
     if (padCheck(Pad1Down)) {
-		px = px - (cos(angle)*5);
-		py = py - (sin(angle)*5);
+		px = px - (c_ang*5);
+		py = py - (s_ang*5);
 	}
 	
 	// Turning buttons
@@ -57,12 +75,30 @@ void update() {
 	if (padCheck(Pad1Circle)) {
 		angle = angle + 0.2;
 	}
+}
+
+void moveSurroundings() {
+	c_ang = cos(angle);
+	s_ang = sin(angle);
 	
-	player = moveLine(player, px, py, ((cos(angle)*6) + px), ((sin(angle)*6) + py));
+	// Transform vertexes relative to the player
+	tx1 = lx1 - px;
+	ty1 = ly1 - py;
+	tx2 = lx2 - px;
+	ty2 = ly2 - py;
+	
+	// Rotate them around the player
+	tz1 = (tx1 * c_ang) + (ty1 * s_ang);
+	tz2 = (tx2 * c_ang) + (ty2 * s_ang);
+	tx1 = (tx1 * s_ang) - (ty1 * c_ang);
+	tx2 = (tx2 * s_ang) - (ty2 * c_ang);
+	
+	line = moveLine(line, (START_X - tx1), (START_Y - tz1), (START_X - tx2), (START_Y - tz2));
+	
 }
 
 void draw() {
-	drawLine(pov);
 	drawLine(player);
+	drawLine(line);
 }
 
